fccddf9a33b555bcd127b1a6a57f0d64cbfea66b
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-05-27 13:00:26 +0300

    fix

--- js/2.js
+++ js/2.js
@@ -85,7 +85,7 @@ function showRandomEmployeeSalary() {
 
   // h1El.textContent = `Доступні кошти - ${balance} грн.`;
   // h1El.textContent = `Доступні кошти - ${balance} грн.`;
-  h1El.innerHTML = `Доступні кошти - ${balance} грн. </audio><audio src="../music/reset.mp3" autoplay preload="auto"> <audio src="../music/У вас є доступні кош.mp3" autoplay preload="auto">`;
+  h1El.innerHTML = `Доступні кошти - ${balance} грн. <audio src="../music/reset.mp3" autoplay preload="auto"></audio> <audio src="../music/У вас є доступні кош.mp3" autoplay preload="auto"></audio>`;
   inputEl.classList.add("active");
   btnEl2.classList.add("active");
   labelEl.classList.add("active");
@@ -104,7 +104,7 @@ function showBalance() {
   //   salary -= value;
 
   if (value === " " || value === 0) {
-    text.innerHTML = `Не коректна сума, спробуйте знову. </audio><audio src="../music/reset.mp3" autoplay preload="auto"> <audio src="../music/Нажаль 0 це не корек.mp3" autoplay preload="auto"></audio>`;
+    text.innerHTML = `Не коректна сума, спробуйте знову.<audio src="../music/reset.mp3" autoplay preload="auto"></audio> <audio src="../music/Нажаль 0 це не корек.mp3" autoplay preload="auto"></audio>`;
   } else if (value > balance) {
     text.textContent = `Недостатньо грошей для проведення операції`;
   } else if (
@@ -126,18 +126,19 @@ function showBalance() {
   ) {
     balance -= value;
     total += value;
-    text.innerHTML = `Знято з рахунку - ${value} грн. Дякуємо за візит. </audio><audio src="../music/reset.mp3" autoplay preload="auto"> <audio src="../music/Дякуємо за візит .mp3" autoplay preload="auto">`;
+    text.innerHTML = `Знято з рахунку - ${value} грн. Дякуємо за візит. <audio src="../music/reset.mp3" autoplay preload="auto"></audio> <audio src="../music/Дякуємо за візит .mp3" autoplay preload="auto"></audio>`;
     h1El.textContent = `Залишок на вашому рахунку - ${balance} грн.`;
 
     a.classList.add("active2");
+
     checkEl.textContent = `Квитанція про зняття коштів ${total} грн`;
   } else {
-    text.innerHTML = `Введіть суму кратну 100, 200, 500, 1000  </audio><audio src="../music/reset.mp3" autoplay preload="auto"> <audio src="../music/Введіть суму кратну .mp3" autoplay preload="auto"></audio>`;
+    text.innerHTML = `Введіть суму кратну 100, 200, 500, 1000  <audio src="../music/reset.mp3" autoplay preload="auto"></audio> <audio src="../music/Введіть суму кратну .mp3" autoplay preload="auto"></audio>`;
   }
 }
 
 function onEnd() {
   a.classList.remove("active2");
   location.href = location.href;
-  end.innerHTML = `Візит <audio src="../music/reset.mp3" autoplay preload="auto">`;
+  end.innerHTML = `Візит <audio src="../music/reset.mp3" autoplay preload="auto"></audio>`;
 }
