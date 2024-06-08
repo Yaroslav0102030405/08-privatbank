0f59fcd96c4a9645ff741e29570a071b7d015419
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-06-08 11:28:50 +0300

    f

--- js/11.js
+++ js/11.js
@@ -68,13 +68,39 @@
 
 // console.log(playerIds);
 
-const numbers = [11, 12, 13, 14, 15, 20, 21, 22, 23, 24];
+// const numbers = [11, 12, 13, 14, 15, 20, 21, 22, 23, 24];
 
-const filterNumbers = numbers.filter((number) => {
-  return number < 15 || number > 23;
-});
+// const filterNumbers = numbers.filter((number) => {
+//   return number < 15 || number > 23;
+// });
 
-console.log(filterNumbers);
+// console.log(filterNumbers);
+
+// const fruits = [
+//   { id: "player-1", name: "apples", quantity: 200, online: true },
+//   { id: "player-2", name: "grapes", quantity: 150, online: false },
+//   { id: "player-3", name: "bananas", quantity: 100, online: true },
+// ];
+
+// const onlineFruits = fruits.filter((fruit) => fruit.online);
+// console.log(onlineFruits);
+
+// const numbers1 = [1, 2, 3, 4, 5, 6];
+
+// const total = numbers1.reduce((acc, number) => acc + number, 0);
+// console.log(total);
+
+// const salary = {
+//   mango: 100,
+//   poly: 200,
+//   ajax: 300,
+// };
+
+// const totalSalary = Object.values(salary).reduce((acc, value) => {
+//   return acc + value;
+// }, 0);
+
+// console.log(totalSalary);
 
 const fruits = [
   { id: "player-1", name: "apples", quantity: 200, online: true },
@@ -82,5 +108,34 @@ const fruits = [
   { id: "player-3", name: "bananas", quantity: 100, online: true },
 ];
 
-const onlineFruits = fruits.filter((fruit) => fruit.online);
-console.log(onlineFruits);
+const totalQuantity = fruits.reduce((acc, fruit) => {
+  return acc + fruit.quantity;
+}, 0);
+
+console.log(totalQuantity);
+
+const cart = [
+  { label: "Apples", price: 100, quantity: 2 },
+  { label: "Bananas", price: 120, quantity: 3 },
+  { label: "Lemons", price: 70, quantity: 4 },
+];
+
+const totalAmount = cart.reduce((acc, { price, quantity }) => {
+  return acc + price * quantity;
+}, 0);
+
+console.log(totalAmount);
+
+const tweeps = [
+  { id: 1, likes: 5, tags: ["js", "node.js"] },
+  { id: 2, likes: 2, tags: ["html", "css"] },
+  { id: 3, likes: 17, tags: ["html", "css"] },
+  { id: 4, likes: 10, tags: ["js", "react"] },
+];
+
+const tagsTweep = tweeps.reduce((acc, tweep) => {
+  acc.push(...tweep.tags);
+
+  return acc;
+}, []);
+console.log(tagsTweep);
