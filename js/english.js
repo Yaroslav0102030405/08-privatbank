774df65cdf63868f8a36f10e81bcc4aa777961f7
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-06-11 16:03:36 +0300

    s

--- js/english.js
+++ js/english.js
@@ -21,6 +21,8 @@ iconBtn2.addEventListener("click", () => {
 });
 
 formSubmit.addEventListener("submit", onFormSubmit);
+inputEl.addEventListener("input", onInput);
+inputEl2.addEventListener("input", onInput2);
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -34,21 +36,83 @@ function onFormSubmit(e) {
   const hello = formElements.hello.value;
   const hi = formElements.hi.value;
 
-  if (hello === "Hello" && hi === "Hi") {
-    success.textContent = "✔️ Правільна відповідь";
-    item3.src = "../audio/Правильна відповідь.mp3";
-    // formSubmit.reset();
-    inputEl.value = `${inputEl.value} ✔️`;
-    inputEl2.value = `${inputEl2.value} ✔️`;
-
-    // item01.classList.add("gray");
+  if (hello === "Hello ✔️" && hi === "Hi ✔️") {
+    // item3.src = "../audio/Правильна відповідь.mp3";
+    onAudioSuccess();
+    // inputEl.value = `${inputEl.value} ✔️`;
+    // inputEl2.value = `${inputEl2.value} ✔️`;
+    success.innerHTML =
+      "✔️ Правільна відповідь <br/>Будь ласка перeходьте до уроку 2";
     item01.classList.add("gray2");
+  } else if (hello === "Hello ✔️") {
+    onAudioError();
+    // item3.src = "../audio/Відповідь не правиль.mp3";
+    // inputEl2.value = `${inputEl2.value} ❌`;
+    onSmileyError(inputEl2);
+    success.textContent = "❌ Не правильна відповідь";
+  } else if (hi === "Hi ✔️") {
+    onAudioError();
+    // item3.src = "../audio/Відповідь не правиль.mp3";
+    // inputEl.value = `${inputEl.value} ❌`;
+    onSmileyError(inputEl);
+    success.textContent = "❌ Не правильна відповідь";
   } else {
-    success.textContent = "❌😈 Не правильна відповідь";
-    item3.src = "../audio/Відповідь не правиль.mp3";
-    // formSubmit.reset();
-
-    inputEl.value = `${inputEl.value} ❌`;
-    inputEl2.value = `${inputEl2.value} ❌`;
+    // item3.src = "../audio/Відповідь не правиль.mp3";
+    // inputEl.value = `${inputEl.value} ❌`;
+    // inputEl2.value = `${inputEl2.value} ❌`;
+    onSmileyError(inputEl);
+    onSmileyError(inputEl2);
+    onAudioError();
+    // success.textContent = "❌ Не правильна відповідь";
   }
 }
+
+const onAudioSuccess = () => {
+  item3.src = "../audio/Правильна відповідь.mp3";
+};
+
+const onAudioError = () => {
+  item3.src = "../audio/Відповідь не правиль.mp3";
+};
+
+function onInput(event) {
+  if ((inputEl.textContent = event.currentTarget.value === "Hello")) {
+    // formSubmit.reset();
+    // inputEl.value = `${inputEl.value} ✔️`;
+    onSmileySuccess(inputEl);
+  }
+}
+
+function onInput2(event) {
+  if ((inputEl2.textContent = event.currentTarget.value === "Hi")) {
+    // inputEl2.value = `${inputEl2.value} ✔️`;
+    onSmileySuccess(inputEl2);
+  }
+}
+
+const onSmileySuccess = (inputSuccess) => {
+  inputSuccess.value = `${inputSuccess.value} ✔️`;
+};
+
+const onSmileyError = (inputError) => {
+  inputError.value = `${inputError.value} ❌`;
+};
+
+//   if (hello === "Hello" && hi === "Hi") {
+//     success.textContent = "✔️ Правільна відповідь";
+//     item3.src = "../audio/Правильна відповідь.mp3";
+//     // formSubmit.reset();
+//     inputEl.value = `${inputEl.value} ✔️`;
+//     inputEl2.value = `${inputEl2.value} ✔️`;
+
+//     // item01.classList.add("gray");
+//     item01.classList.add("gray2");
+//   } else {
+//     success.textContent = "❌😈 Не правильна відповідь";
+//     item3.src = "../audio/Відповідь не правиль.mp3";
+//     // formSubmit.reset();
+
+//     inputEl.value = `${inputEl.value} ❌`;
+//     inputEl2.value = `${inputEl2.value} ❌`;
+//   }
+// }
